2b580a96b2288f01fa85b43492ee3c7fc3466c53
Patrick Godschalk <argure@donttrustrobots.nl>
2015-08-27 18:23:45 +0200

    Refactor the root directory

--- slack-register.php
+++ slack-register.php
@@ -1,52 +1,40 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]> <html class="en no-js ie6 oldie ie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="en no-js ie7 oldie ie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="en no-js ie8 oldie ie" lang="en"> <![endif]-->
-<!--[if gt IE 8]> <html class="en no-js newie ie" lang="en"> <![endif]-->
-<!--[if !IE]><!-->
-<html class="en no-js" lang="en">
-<!--<![endif]-->
-
-<head>
-    <meta charset="UTF-8">
-    <title>Spectre Fleet - Register for our Slack page now!</title>
-    <?php include 'includes/static-header.php';?>
-</head>
-
-<body id="landing">
-        <header class="main-content noprint" id="mainheader-responsive">
-            <div>
-                <a id="mainlogo" href="/index"><img alt="Spectre Fleet" src="/img/layout/logo.png" /></a>
-            </div>
-            <a id="nav-toggle" href="#navigation"><span aria-hidden="true" class="nav-toggle-icon"></span><span class="visuallyhidden">Menu</span></a>
-        </header>
-
-        <section id="navigation-responsive">
-            <nav id="mainnav">
-                <ul class="clearfix">
-                    <li><a href="/index">Frontpage</a></li>
-                    <li><a href="/news/">Spectre News</a></li>
-                    <li><a href="/commanders">Our Commanders</a></li>
-                    <li><a href="/doctrines">Our Doctrines</a></li>
-                    <li class="selected"><a href="#">Fleet Tools</a>
-                        <ul class="i3">
-                            <li class="col1">
-                                <a href="/feedback">Fleet Feedback Form</a>
-                            </li>
-                            <li class="col1">
-                                <a href="https://spectrefleet.slack.com/messages/general/" target="_blank">Spectre Slack</a>
-                            </li>
-                            <li class="col1">
-                                <a href="/slack-register">Slack Registration</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        </section>
-
-        <main class="main-content" id="mainstuff" role="main">
-
-<iframe src="register.php" width="100%" height="883" frameborder="0"></iframe>
-
-<?php include 'includes/footer.php';?>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Spectre Fleet - Register for our Slack page now!</title>
+		<?php include "includes/static-header.php"; ?>
+	</head>
+	<body id="landing">
+		<header class="main-content noprint" id="mainheader-responsive">
+			<div>
+				<a id="mainlogo" href="/index"><img alt="Spectre Fleet" src="/img/layout/logo.png"></a>
+			</div>
+			<a id="nav-toggle" href="#navigation"><span aria-hidden="true" class="nav-toggle-icon"></span><span class="visuallyhidden">Menu</span></a>
+		</header>
+		<section id="navigation-responsive">
+			<nav id="mainnav">
+				<ul class="clearfix">
+					<li><a href="/index">Frontpage</a></li>
+					<li><a href="/news/">Spectre News</a></li>
+					<li><a href="/commanders">Our Commanders</a></li>
+					<li><a href="/doctrines">Our Doctrines</a></li>
+					<li class="selected"><a href="#">Fleet Tools</a>
+						<ul class="i3">
+							<li class="col1">
+								<a href="/feedback">Fleet Feedback Form</a>
+							</li>
+							<li class="col1">
+								<a href="https://spectrefleet.slack.com/messages/general/" target="_blank">Spectre Slack</a>
+							</li>
+							<li class="col1">
+								<a href="/slack-register">Slack Registration</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+		</section>
+		<main class="main-content" id="mainstuff" role="main">
+			<iframe src="register.php" width="100%" height="883"></iframe>
+		<?php include "includes/footer.php"; ?>
