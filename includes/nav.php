4bca2fc0ecb1c74a9266728fbd5bba6786a00645
Spectre Fleet <admin@spectrefleet.com>
2015-09-01 21:33:20 -0400

    Navigation now re-used as include

---
+++ includes/nav.php
@@ -0,0 +1,33 @@
+<header class="main-content noprint" id="mainheader-responsive">
+    <div>
+        <a id="mainlogo" href="/index"><img alt="Spectre Fleet" src="/img/layout/logo.png"></a>
+    </div>
+    <a id="nav-toggle" href="#navigation"><span aria-hidden="true" class="nav-toggle-icon"></span><span class="visuallyhidden">Menu</span></a>
+</header>
+<section id="navigation-responsive">
+    <nav id="mainnav">
+        <ul class="clearfix">
+            <li><a href="/">Frontpage</a></li>
+            <li><a href="/news/">Spectre News</a></li>
+            <li class="selected"><a href="/commanders">Our Commanders</a></li>
+            <li><a href="/doctrines">Our Doctrines</a></li>
+            <li><a href="#">Fleet Tools</a>
+                <ul class="i3">
+                    <li class="col1">
+                        <a href="/feedback">Fleet Feedback Form</a>
+                    </li>
+                    <li class="col1">
+                        <a href="https://spectrefleet.slack.com/messages/general/" target="_blank">Spectre Slack</a>
+                    </li>
+                    <li class="col1">
+                        <a href="/slack-register">Slack Registration</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="/stream">Stream</a>
+            </li>
+        </ul>
+    </nav>
+</section>
+
---
+++ stream.php
@@ -0,0 +1,22 @@
+<?php
+require_once "core/IGBFunctions.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Spectre Fleet Commanders - The largest and best NPSI Community in EVE Online</title>
+        <?php include "includes/static-header.php"; ?>
+    </head>
+    <body id="landing" class="animate">
+        <?php include "includes/nav.php"; ?>
+       <main class="main-content" id="mainstuff" role="main">
+            <section id="fc">
+                <div class="container">
+                    <iframe width="1024" height="630" src="https://www.youtube.com/embed/5qCI-JGMcPQ" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </section>
+            <div class="clearfix"></div>
+        </main>
+<?php include "includes/footer.php"; ?>
