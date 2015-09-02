7354b0a0dc9180e1d295ac8a8e7aa96bf332df80
Ibn Khatab <arron@arronchapman.com>
2015-09-02 11:13:20 -0700

    Adding teamspeak page with filler text

---
+++ teamspeak.php
@@ -0,0 +1,118 @@
+<?php
+require_once "core/IGBFunctions.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Spectre Fleet - The largest and best NPSI Community in EVE Online</title>
+        <?php include "includes/static-header.php"; ?>
+    </head>
+    <body id="landing">
+        <?php include "includes/nav.php"; ?>
+       <main class="main-content" id="mainstuff" role="main">
+            <section id="intro">
+                <div class="texture"></div>
+                <div id="vidcontainer">
+                    <div class="inner">
+                        <video autoplay id="vid2000" loop poster="/img/layout/cover.jpg">
+                        <source src="/assets/eve_background.webm" type="video/webm"></video>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row titletext">
+                            <h2>Spectre Fleet</h2>
+                            <h3>Gaming Community</h3>
+                            <?php if (checkIGB()) { ?>
+                                <?php if (checkTrust() == 0) { ?>
+                                    <div align="center">
+                                        <div class="col-sm-12">
+                                            <div style="padding: 60px 0 0;">
+                                                <a class="button--arrow landing red" href="#" style="font-family: 'Arial', sans-serif; font-size: 0.8em; white-space: nowrap;" onclick="<?php printRequestTrust(); ?>"><span class="button--arrow-text">Install Spectre Fleet as trusted</span></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php } else { ?>
+                                    <div align="center">
+                                        <div class="col-sm-12">
+                                            <div style="padding: 60px 0 0;">
+                                                <a class="button--arrow landing red" href="#" style="font-family: 'Arial', sans-serif; font-size: 0.8em; white-space: nowrap;" onclick="<?php printJoinChannel(); ?>"><span class="button--arrow-text">X up now in Spectre Fleet!</span></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php } ?>
+                            <?php } ?>
+                    </div>
+                </div>
+            </section>
+            <section id="what">
+                <nav id="section-menu">
+                    <div class="container">
+                        <ul class="nav">
+                            <li>
+                                <a href="/index"><img height="29" src="/img/layout/logo.png" style="margin-top: -5px;" alt="Spectre Logo" width="85"></a>
+                            </li>
+                            <li>
+                                <a href="#warships">Section 1</a>
+                            </li>
+                            <li>
+                                <a href="#tanks">Section 2</a>
+                            </li>
+                            <li>
+                                <a href="#everyone">Section 3</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h2 style="text-align: center !important; margin-top: 40px !important;">Looking to participate in Spectre Fleet's Wargaming Branch? Look no further!</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <p></p>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+                <section id="spaceships">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6">
+								<p>
+									Section 1 text
+								</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="play">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6 col-sm-offset-6">
+                                <div class="signup">
+								<p>
+									Section 2 text
+								</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="spaceships">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6">
+								<p>
+									Section 3 text
+								</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+           </main>
+           <?php include "includes/footer.php"; ?>
