600cde13ff434bc3c33cdded6e31204666670b4b
BuildTools <unconfigured@null.spigotmc.org>
2015-09-01 17:59:47 -0700

    Fixed commanders page formatting again.

--- includes/profiles/douglas.php
+++ includes/profiles/douglas.php
@@ -1,4 +1,4 @@
-<h4>Douglas Aurelius</h4>
+<h4>Douglas</h4>
 <p>
-    <img src="//image.eveonline.com/Character/91089809_256.jpg" alt="Douglas Aurelius" onclick="<?php printShowInfo(1377, 91089809); ?>">
+    <img src="//image.eveonline.com/Character/91089809_256.jpg" alt="Douglas" onclick="<?php printShowInfo(1377, 91089809); ?>">
 </p>
