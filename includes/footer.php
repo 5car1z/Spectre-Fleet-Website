258d5b253d9954b5e19ef4a8d0f7e3c5694a0e9c
Ibn Khatab <arron@arronchapman.com>
2015-09-01 21:10:25 -0700

    Changing the YT channelID again

--- includes/footer.php
+++ includes/footer.php
@@ -13,7 +13,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/core/BrowserCommon.php';
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script> | 
 <script src="https/apis.google.com/js/platform.js"></script>
 
-<div class="g-ytsubscribe" data-channelid="UCKJ4q8Z5ekymgk2a_mPdIRg" data-layout="default" data-count="hidden"></div>
+<div class="g-ytsubscribe" data-channelid="SpectreFleetGaming" data-layout="default" data-count="hidden"></div>
 							</p>
                             <p>EVE Online and the EVE logo are the registered trademarks of CCP hf. All rights are reserved worldwide. All other trademarks are the property of their respective owners. EVE Online, the EVE logo, EVE and all associated logos and designs are the intellectual property of CCP hf. All artwork, screenshots, characters, vehicles, storylines, world facts or other recognizable features of the intellectual property relating to these trademarks are likewise the intellectual property of CCP hf. CCP hf. has granted permission to Spectre Fleet to use EVE Online and all associated logos and designs for promotional and information purposes on its website but does not endorse, and is not in any way affiliated with, Spectre Fleet. CCP is in no way responsible for the content on or functioning of this website, nor can it be liable for any damage arising from the use of this website. <a href="legal.txt">Legal information</a></p>
                         </div>
