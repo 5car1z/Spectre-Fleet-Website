9276cb9eb4f3aff4049f93c4006c4a850ea1375e
Spectre Fleet <admin@spectrefleet.com>
2015-09-01 21:40:03 -0400

    moved all nav to include

--- slack-register.php
+++ slack-register.php
@@ -6,35 +6,7 @@
         <?php include "includes/static-header.php"; ?>
     </head>
     <body id="landing">
-        <header class="main-content noprint" id="mainheader-responsive">
-            <div>
-                <a id="mainlogo" href="/index"><img alt="Spectre Fleet" src="/img/layout/logo.png"></a>
-            </div>
-            <a id="nav-toggle" href="#navigation"><span aria-hidden="true" class="nav-toggle-icon"></span><span class="visuallyhidden">Menu</span></a>
-        </header>
-        <section id="navigation-responsive">
-            <nav id="mainnav">
-                <ul class="clearfix">
-                    <li><a href="/index">Frontpage</a></li>
-                    <li><a href="/news/">Spectre News</a></li>
-                    <li><a href="/commanders">Our Commanders</a></li>
-                    <li><a href="/doctrines">Our Doctrines</a></li>
-                    <li class="selected"><a href="#">Fleet Tools</a>
-                        <ul class="i3">
-                            <li class="col1">
-                                <a href="/feedback">Fleet Feedback Form</a>
-                            </li>
-                            <li class="col1">
-                                <a href="https://spectrefleet.slack.com/messages/general/" target="_blank">Spectre Slack</a>
-                            </li>
-                            <li class="col1">
-                                <a href="/slack-register">Slack Registration</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        </section>
-        <main class="main-content" id="mainstuff" role="main">
+    <?php include "includes/nav.php"; ?>
+       <main class="main-content" id="mainstuff" role="main">
             <iframe src="register.php" width="100%" height="883"></iframe>
         <?php include "includes/footer.php"; ?>
