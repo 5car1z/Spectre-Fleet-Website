e48ea5289a9e73d52745127fcb01672d097c6a26
Ibn Khatab <arron@arronchapman.com>
2015-10-12 23:26:33 -0700

    Adding contact us to mail

--- includes/nav.php
+++ includes/nav.php
@@ -31,13 +31,15 @@
 			<li <?php getPage("other-games"); ?> ><a href="/other-games">Other Games</a>
 			</li>
             <li <?php getPage("feedback");getPage("slack-register");getPage("teamspeak"); ?> ><a href="#">Tools</a>
-                <ul class="i6">
+                <ul class="i8">
 					<li><a href="/feedback">Feedback Form</a> </li>
 					<li class="col1"><a href="/teamspeak">Teamspeak 3</a> </li>
-					<li class="col1"><hr><li>
+					<li class="col1"><hr></li>
 					<li class="col1"><a href="https://spectrefleet.slack.com/messages/general/" target="_blank">Open Slack</a></li>
 					<li class="col1"><a href="/slack-about">About Slack</a></li>
 					<li class="col1"><a href="/slack-register">Register</a></li>
+					<li class="col1"><hr></li>
+					<li class="col1"><a href="mailto:staff@spectrefleet.com">Contact Us</a></li>
 					</li>
                 </ul>
             </li>
