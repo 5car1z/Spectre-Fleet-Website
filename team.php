6e1d9e0a175eef00c6c2663bd3d7a21fcc21e469
Ibn Khatab <arron@arronchapman.com>
2015-10-12 23:09:30 -0700

    Our Team Layout Changes

--- team.php
+++ team.php
@@ -32,6 +32,15 @@ $team[] = array('id' => 94633294, 'name' => 'Elo', 'title' => 'CPO');
                         </div>
                     </div>
                     <div class="row">
+						<div class="col-sm-6-command">
+							<h4>Jayne</h4>
+							<h1 style="margin-left: -10px;">President</h1>
+							<p>
+								<img src="//image.eveonline.com/Character/91688663_256.jpg" style="margin-top: -25px;" alt="Jayne" onclick="<?php printShowInfo(1377,91688663); ?>">
+							</p>
+						</div>
+					</div>
+                    <div class="row">
 					<?php
 						foreach($team as $member)
 						{
@@ -39,7 +48,7 @@ $team[] = array('id' => 94633294, 'name' => 'Elo', 'title' => 'CPO');
 								<h4><?php echo $member['name']; ?></h4>
 								<h1 style="margin-left: -10px;"><?php echo $member['title']; ?></h1>
 								<p>
-									<img src="//image.eveonline.com/Character/<?php echo $member['id']; ?>_256.jpg" alt="<?php echo $member['name']; ?>" onclick="<?php printShowInfo(1377,$member['id']); ?>">
+									<img src="//image.eveonline.com/Character/<?php echo $member['id']; ?>_256.jpg" style="margin-top: -25px;" alt="<?php echo $member['name']; ?>" onclick="<?php printShowInfo(1377,$member['id']); ?>">
 								</p>
 							</div>
 							<?php
