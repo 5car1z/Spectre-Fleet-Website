1f94cfc2bb9383100bd783e4817be33dd570f066
Jayne Fillon <justinthomasconroy@gmail.com>
2015-10-12 22:18:42 -0600

    Update static-header.php

--- includes/static-header.php
+++ includes/static-header.php
@@ -1,7 +1,7 @@
 <meta content="Spectre Fleet" name="author">
 <link rel="icon" type="image/png" href="/favicon.png">
-<meta content="Spectre Fleet is the biggest NPSI community in the MMO EVE Online. You can join us by joining our in game channel Spectre Fleet and whenever there's a fleet, X up!" name="description">
-<meta name="keywords" content="Spectre,Fleet,EVE,Online,MMORPG,NPSI,SpectreFleet,Games,Space,Spaceships,Explosions,MMO,RPG,Multiplayer">
+<meta content="Spectre Fleet is an online Gaming Community that originated in Eve Online and partcipates in a wide variety of competitive multiplayer games. Come join the largest and best public gaming community!" name="description">
+<meta name="keywords" content="Spectre,Fleet,EVE,Online,NPSI,SpectreFleet,Games,MMO,Multiplayer,Online,Gaming,Community">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="/css/style.css" rel="stylesheet">
 <link href="/lib/Font-Awesome/css/font-awesome.min.css" rel="stylesheet">
