b10f46d81a35b3f625aad27276030cf615fd5c53
Arron Chapman <arron@arronchapman.com>
2015-10-22 22:21:10 -0700

    Final bug fix

--- public/application/views/users/index.php
+++ public/application/views/users/index.php
@@ -1,8 +1,9 @@
 <h2><?php echo count($user); ?> Users</h2>
 <ul>
-	<php foreach($u in $user)
+	<?php
+	foreach($u in $user)
 	{
-		echo "<li><a href='/user/" . $u['ID'] . "'>" . $u['DisplayName'] . '</a></li>';
+		echo '<li><a href="/user/' . $u['ID'] . '">' . $u['DisplayName'] . '</a></li>';
 	}
 	?>
 </ul>
