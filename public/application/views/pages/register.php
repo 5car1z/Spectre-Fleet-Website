99a2e97fd6f9ec2e3efe82d039784b0175e6aaeb
Arron Chapman <arron@arronchapman.com>
2015-10-22 22:35:23 -0700

    Updating registration form

--- public/application/views/pages/register.php
+++ public/application/views/pages/register.php
@@ -1,11 +1,9 @@
-<?php
-session_start();
 
-form_open('user/register', null, array('registrationpage' => 'true'));
-echo '<br />';
-form_input('Username', '');echo '<br />';
-form_input('Character Name', '');echo '<br />';
-form_password('Password', '');echo '<br />';
-form_input('Email Addresss', '');echo '<br />';
-form_submit('submit', 'Register');echo '<br />';
-form_close();
+<form method="post" action="/user/create/">
+Username: <input type="text" name="username"><br />
+Password: <input type="password" name="password"><br />
+Confirm Password: <input type="password" name="confirmpassword"><br />
+<br />
+Character Name: <input type="text" name="character"><br />
+Email Address: <input type="email" name="email"><br />
+</form>
