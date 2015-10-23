be35ffc5c197995c909fee13dc810c354e777c54
Arron Chapman <arron@arronchapman.com>
2015-10-22 22:22:07 -0700

    Array order change

--- public/application/views/users/index.php
+++ public/application/views/users/index.php
@@ -1,7 +1,7 @@
 <h2><?php echo count($user); ?> Users</h2>
 <ul>
 	<?php
-	foreach($u in $user)
+	foreach($user as $u)
 	{
 		echo '<li><a href="/user/' . $u['ID'] . '">' . $u['DisplayName'] . '</a></li>';
 	}
